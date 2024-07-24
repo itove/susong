5be3ba3b248406833373c10c171673645424e4fc
Al Zee <z@alz.ee>
2024-07-24 15:17:57 +0800

    nth

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Service\Wx;
+use App\Entity\Feedback;
+
+#[Route('/api')]
+class ApiController extends AbstractController
+{
+    #[Route('/feedback', methods: ['POST'])]
+    public function feedback(Request $request): Response
+    {
+        $params = $request->toArray();
+        $firstname = $params['firstname'];
+        // $lastname = $params['lastname'];
+        $phone = $params['phone'];
+        $email = $params['email'];
+        $title = $params['title'];
+        $body = $params['body'];
+        // $country = $params['country'];
+        
+        $em = $this->data->getEntityManager();
+        $f = new Feedback();
+        $f->setFirstname($firstname);
+        // $f->setLastname($lastname);
+        $f->setPhone($phone);
+        $f->setEmail($email);
+        $f->setTitle($title);
+        $f->setBody($body);
+        // $f->setCountry($country);
+        $em->persist($f);
+        $em->flush();
+
+        $data = [
+            'code' => 0,
+            'msg' => 'ok',
+        ];
+
+        return $this->json($data);
+    }
+
+    #[Route(path: '/wxconfig', name: 'api_wx_config', methods: ['GET'])]
+    public function wxConfig(Wx $wx)
+    {
+        $nonce = '123';
+        $timestamp = '234';
+        $ticket = $wx->getJSTicket();
+        $url = 'https://url.com';
+
+        $sig = $wx->getJSSignature($ticket, $nonce, $timestamp, $url);
+
+        $data = [
+            'appId' = '',
+            'timestamp' = $timestamp,
+            'nonceStr' = $nonce,
+            'signature' = $sig,
+        ];
+
+        return $this->json($data);
+    }
+}
